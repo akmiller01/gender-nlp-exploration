bb24c305231cf7d062dd8c791727b4f8731fb306
akmiller01 <alex.k.miller@gmail.com>
2024-06-18 15:44:59 -0400

    Change back to multiclass

--- code/train_iati_gender_bootstrap_weighted.py
+++ code/train_iati_gender_bootstrap_weighted.py
@@ -21,6 +21,9 @@ import numpy as np
 
 from typing import Optional, Tuple, Union
 
+card = "alex-miller/ODABert"
+tokenizer = AutoTokenizer.from_pretrained(card, model_max_length=512)
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
 def weighted_forward_bert(
     self,
@@ -96,20 +99,34 @@ def weighted_forward_bert(
 dataset = load_dataset("alex-miller/curated-iati-gender-equality")
 
 unique_labels = [
-    "No gender equality objective",
     "Significant gender equality objective",
     "Principal gender equality objective"
 ]
 id2label = {i: label for i, label in enumerate(unique_labels)}
 label2id = {id2label[i]: i for i in id2label.keys()}
 
+def preprocess_function(example):
+    labels = [0. for i in range(len(unique_labels))]
+    for label in unique_labels:
+        if example['label'] == label:
+            labels[label2id[label]] = 1.
+
+    example = tokenizer(example['text'], truncation=True)
+    example['labels'] = labels
+    return example
+
+dataset = dataset.map(preprocess_function, remove_columns=['text', 'label'])
+
 weight_list = list()
 total_rows = dataset['train'].num_rows + dataset['test'].num_rows
 print("Weights:")
 for label in unique_labels:
-    positive_filtered_dataset = dataset.filter(lambda example: label == example['label'])
+    label_idx = label2id[label]
+    positive_filtered_dataset = dataset.filter(lambda example: example['labels'][label_idx] == 1.)
+    negative_filtered_dataset = dataset.filter(lambda example: example['labels'][label_idx] == 0.)
     pos_label_rows = positive_filtered_dataset['train'].num_rows + positive_filtered_dataset['test'].num_rows
-    label_weight = total_rows / pos_label_rows
+    neg_label_rows = negative_filtered_dataset['train'].num_rows + negative_filtered_dataset['test'].num_rows
+    label_weight = neg_label_rows / pos_label_rows
     weight_list.append(label_weight)
     print("{}: {}".format(label, label_weight))
 
@@ -117,29 +134,25 @@ device = "cuda:0" if torch.cuda.is_available() else "cpu"
 weights = torch.tensor(weight_list)
 weights = weights.to(device)
 
-card = "alex-miller/ODABert"
-tokenizer = AutoTokenizer.from_pretrained(card, model_max_length=512)
-data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-
-def preprocess_function(example):
-    example['label'] = label2id[example['label']]
-    example = tokenizer(example['text'], truncation=True)
-    return example
-
 clf_metrics = evaluate.combine(["accuracy", "f1", "precision", "recall"])
-def compute_metrics(eval_pred):
-    predictions, labels = eval_pred
-    predictions = np.argmax(predictions, axis=1)
-    return clf_metrics.compute(predictions=predictions, references=labels, average='macro')
 
-tokenized_data = dataset.map(preprocess_function, remove_columns=["text"])
+def sigmoid(x):
+   return 1/(1 + np.exp(-x))
+
+
+def compute_metrics(eval_pred):
+   predictions, labels = eval_pred
+   predictions = sigmoid(predictions)
+   predictions = (predictions > 0.5).astype(int).reshape(-1)
+   return clf_metrics.compute(predictions=predictions, references=labels.astype(int).reshape(-1))
+
 
 model = AutoModelForSequenceClassification.from_pretrained(
     card,
     num_labels=len(id2label.keys()), 
     id2label=id2label,
     label2id=label2id, 
-    problem_type="single_label_classification"
+    problem_type="multi_label_classification"
 )
 model.forward = types.MethodType(weighted_forward_bert, model)
 model.class_weights = weights
@@ -162,8 +175,8 @@ training_args = TrainingArguments(
 trainer = Trainer(
     model=model,
     args=training_args,
-    train_dataset=tokenized_data['train'],
-    eval_dataset=tokenized_data['test'],
+    train_dataset=dataset['train'],
+    eval_dataset=dataset['test'],
     tokenizer=tokenizer,
     data_collator=data_collator,
     compute_metrics=compute_metrics,
